794198e3c53994b88bdbbd79237d422a401b0152
beratcan97 <beratcan_acil@hotmail.se>
2019-10-17 23:13:40 +0200

    first commit

--- src/app/gameScreen/blackjack.component.ts
+++ src/app/gameScreen/blackjack.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-blackjack',
   templateUrl: './blackjack.component.html',
@@ -20,21 +21,22 @@ export class BlackjackComponent implements OnInit {
 
   playerValue: number = 0;
   dealerValue: number = 0;
+
   constructor() { }
 
   ngOnInit() {
-    setTimeout(() => { this.isLoaded = true }, 3000)
-  }
-
-  gameupdater(): void {
-    this.playerAndDealerCardsValueCount();
-    this.checkBlackjackAndBust();
+    this.playEffect("assets/soundEffects/mixCards.m4a");
+    setTimeout(() => {
+      this.isLoaded = true 
+    }, 2000)
   }
 
   hit(): void {
-    this.playerCards.push(this.randomCard(1)[0]);
+    this.playEffect("assets/soundEffects/deal1Card.m4a");
 
-    this.gameupdater();
+    this.playerCards.push(this.randomCard(1)[0]);
+    this.playerCardsValueCount();
+    this.checkBlackjackAndBust();
   }
 
   stand(): void {
@@ -43,7 +45,7 @@ export class BlackjackComponent implements OnInit {
     // Dealers move
     while (this.dealerValue < this.playerValue) {
       this.dealerCards.push(this.randomCard(1)[0]);
-      this.playerAndDealerCardsValueCount();
+      this.dealerCardsValueCount();
     }
 
     if (this.dealerValue === this.playerValue) {
@@ -66,6 +68,8 @@ export class BlackjackComponent implements OnInit {
   }
 
   restartGame(): void {
+    this.dealerValue = 0;
+    this.playerValue = 0;
     this.cards = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52'];
     this.dealerCards = [];
     this.playerCards = [];
@@ -75,7 +79,7 @@ export class BlackjackComponent implements OnInit {
     this.dealerValue = 0;
 
     this.startNewGame();
-    this.gameupdater();
+    this.dealerCardsValueCount();
   }
 
   checkBlackjackAndBust(): void {
@@ -94,33 +98,62 @@ export class BlackjackComponent implements OnInit {
     }
   }
 
-  playerAndDealerCardsValueCount(): void {
-    let tmpPlayer: number = 0;
-    this.playerCards.forEach(card => {
-      tmpPlayer = (tmpPlayer + this.cardsValueCounter(card));
-    });
-    this.playerValue = tmpPlayer;
-
-    let tmpDealer: number = 0;
-    this.dealerCards.forEach(card => {
-      tmpDealer = (tmpDealer + this.cardsValueCounter(card));
-    });
-    this.dealerValue = tmpDealer;
-  }
-
   endGame(message): void {
     this.message = message;
     this.gameRunning = false;
   }
 
-  cardsValueCounter(index: number): number {
+  dealerCardsValueCount(): void {
+    let tmpDealer: number = 0;
+    let tmpADeck = [];
+    this.dealerCards.forEach(card => {
+      if (card < 49) {
+        tmpDealer = (tmpDealer + this.cardValueCounter(card, this.dealerValue));
+      } else {
+        tmpADeck.push(card);
+      }
+    });
+    tmpADeck.forEach(card => {
+      tmpDealer = (tmpDealer + this.aCardValueCounter(this.dealerValue));
+    });
+    this.dealerValue = tmpDealer;
+  }
+
+  playerCardsValueCount(): void {
+    let tmpPlayer: number = 0;
+    let tmpADeck = [];
+    this.playerCards.forEach(card => {
+      if (card < 49) {
+        tmpPlayer = (tmpPlayer + this.cardValueCounter(card, this.playerValue));
+      } else {
+        tmpADeck.push(card);
+      }
+    });
+    tmpADeck.forEach(card => {
+      tmpPlayer = (tmpPlayer + this.aCardValueCounter(this.playerValue));
+    });
+    this.playerValue = tmpPlayer;
+  }
+
+  aCardValueCounter(userValue): number {
+    if ((userValue + 11) == 21) {
+      return 11
+    } else if ((userValue + 11) < 21) {
+      return 11
+    }
+    else {
+      return 1
+    }
+  }
+  
+  cardValueCounter(index: number, user): number {
     let toatelValue: number = 0;
     if (index >= 1 && index <= 4) {
       toatelValue = toatelValue + 2;
     } else if (index >= 5 && index <= 8) {
       toatelValue = toatelValue + 3;
     } else if (index >= 9 && index <= 12) {
-      toatelValue = toatelValue + 4;
+      toatelValue = toatelValue + 4; 
     } else if (index >= 13 && index <= 16) {
       toatelValue = toatelValue + 5;
     } else if (index >= 17 && index <= 20) {
@@ -135,18 +168,22 @@ export class BlackjackComponent implements OnInit {
       toatelValue = toatelValue + 10;
     } else {
       //A
-      toatelValue = toatelValue + 1;
+      console.log("Error");
     }
-
     return toatelValue;
   }
 
   startNewGame(): void {
+    this.playEffect("assets/soundEffects/deal4Cards.m4a");
+
     this.gameRunning = true;
     this.message = '';
     this.dealerCards = this.randomCard(2);
     this.playerCards = this.randomCard(2);
-    this.gameupdater();
+
+    this.playerCardsValueCount();
+    this.dealerCardsValueCount();
+    this.checkBlackjackAndBust();
   }
 
   randomCard(number) {
@@ -159,5 +196,12 @@ export class BlackjackComponent implements OnInit {
       this.cards.splice(cardIndex, 1);
     }
     return randomCards;
+  }
+
+  playEffect(src){
+    let audio = new Audio();
+    audio.src = src;
+    audio.load();
+    audio.play();
   }
 }
